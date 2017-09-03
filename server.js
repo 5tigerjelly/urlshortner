51a0cc2e16157bda0e304275853b5bb7786e7821
Bum Mook Oh <bmo5@uw.edu>
2017-09-03 22:13:16 +0900

    add front end

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
 DB = {};
 
+app.get('/:url', function (req, res) {
+  console.log("in");
+  var longUrl = getUrl(req.params.url);
+  res.json({longUrl:longUrl});
+})
+
 function setUrl(fullUrl){
-  var randomUrl = makeid();
-  while(DB[randomUrl]){
-    randomUrl = makeid();
+  if(DB[fullUrl]){
+    return DB[fullUrl];
+  }else{
+    var randomUrl = makeid();
+    while(DB[randomUrl]){
+      randomUrl = makeid();
+    }
+    DB[randomUrl] = fullUrl;
+    return randomUrl;
   }
-  DB[randomUrl] = fullUrl;
-  return randomUrl;
+
 }
 
 function getUrl(shortUrl){
@@ -29,3 +45,11 @@ var short = setUrl("longurl.com");
 console.log(short);
 console.log(getUrl(short));
 console.log(DB);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
