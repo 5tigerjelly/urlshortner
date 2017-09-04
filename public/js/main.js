8cbe7757ef2afaf93b0535530cd08f90aabc033f
Bum Mook Oh <bmo5@uw.edu>
2017-09-04 15:30:23 +0900

    add url validation

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,40 @@
-$(function(){
+$(function() {
     $('#resultsBox').hide();
-    $('#shortUrlform').submit(function(event){
+    $('#shortUrlform').submit(function(event) {
         event.preventDefault();
         setUrl($('#formUrl').val());
+        // if ($('#formUrl')[0].validity.valid) {
+        //
+        // } else {
+        //     //not valid url
+        //     console.log('not valid url');
+        // }
     });
 });
 
-function setUrl(fullUrl){
-  $.ajax({
-   url: '/',
-   type: 'PUT',
-   dataType: "json",
-   data: { url: fullUrl },
-   success: function(data) {
-     var shortUrl = window.location.href + data.shortUrl;
-     $('#shortUrlLink').text(shortUrl);
-     $("a").attr("href", shortUrl);
-     $('#formUrl').val('');
-     $('#resultsBox').fadeIn();
-     copyToClipboard(shortUrl);
-   }
- });
+function setUrl(fullUrl) {
+    $.ajax({
+        url: '/',
+        type: 'PUT',
+        dataType: "json",
+        data: {
+            url: fullUrl
+        },
+        success: function(data) {
+            var shortUrl = window.location.href + data.shortUrl;
+            $('#shortUrlLink').text(shortUrl);
+            $("a").attr("href", shortUrl);
+            $('#formUrl').val('');
+            $('#resultsBox').fadeIn();
+            copyToClipboard(shortUrl);
+        }
+    });
 }
 
 function copyToClipboard(element) {
-  var $temp = $("<input>");
-  $("body").append($temp);
-  $temp.val(element).select();
-  document.execCommand("copy");
-  $temp.remove();
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val(element).select();
+    document.execCommand("copy");
+    $temp.remove();
 }
